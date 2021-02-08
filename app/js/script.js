bcf86a43d28b3871b7710df8ca37c5c673807396
Foisal <workkaj01@gmail.com>
2021-02-08 19:54:02 +0600

    Sliding Mobile Menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,7 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const mobile__menu = document.querySelector('.mobile__menu');
 
 btnHamburger.addEventListener('click', function (){
   console.log('open hamburger');
@@ -10,12 +11,16 @@ btnHamburger.addEventListener('click', function (){
     header.classList.remove('overlay');
     overlay.classList.remove('fade-in');
     overlay.classList.add('fade-out');
+    mobile__menu.classList.remove('fade-in');
+    mobile__menu.classList.add('fade-out');
   }
   else{//Open hamburger menu
     header.classList.add('open');
     header.classList.add('overlay');
     overlay.classList.add('fade-in');
     overlay.classList.remove('fade-out');
+    mobile__menu.classList.add('fade-in')
+    mobile__menu.classList.remove('fade-out');
   }
 });
 
